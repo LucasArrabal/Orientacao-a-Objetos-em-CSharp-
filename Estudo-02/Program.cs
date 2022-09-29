b516e3e1f0385c374f730a922d14f1705480f808
LucasArrabal <101684353+LucasArrabal@users.noreply.github.com>
2022-09-29 10:58:00 -0300

    Update Program.cs

--- Estudo-02/Program.cs
+++ Estudo-02/Program.cs
@@ -1,4 +1,20 @@
-﻿using Estudo_02.Models;
+using Estudo_02.Models;
 
-Pessoa p1 = new Pessoa("Lucas", 19);
+Pessoa p1 = new Pessoa("Lucas","Arrabal", 19);
+Pessoa p2 = new Pessoa("Jopa","Arrabal", 19);
+Pessoa p3 = new Pessoa("Lucio","Arrabal", 19);
+Pessoa p4 = new Pessoa("Cesar","Arrabal", 19);
 p1.Apresentar();
+
+Curso ADS = new Curso();
+ADS.Nome = "ADS";
+
+ADS.Alunos = new List<Pessoa>();
+
+ADS.AdicionarAluno(p1);
+ADS.AdicionarAluno(p2);
+ADS.AdicionarAluno(p3);
+ADS.AdicionarAluno(p4);
+
+ADS.ListarAlunos();
+ADS.ObterNumAlunos();
