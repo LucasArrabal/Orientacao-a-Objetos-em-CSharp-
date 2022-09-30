1c41462d258fe71dd769c35ff33ba69bec729b1a
LucasArrabal <101684353+LucasArrabal@users.noreply.github.com>
2022-09-30 18:37:34 -0300

    Add files via upload

---
+++ FilaPilha/Program.cs
@@ -0,0 +1,45 @@
+﻿ static void Fila()
+{
+    Queue<int> fila = new Queue<int>();
+    fila.Enqueue(1);
+    fila.Enqueue(2);
+    fila.Enqueue(3);
+    fila.Enqueue(4);
+
+    foreach (var item in fila)
+    {
+        Console.WriteLine(item);
+    }
+
+    Console.WriteLine($"Removendo \n{fila.Dequeue()}");
+    foreach (var item in fila)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+Fila();
+
+Console.WriteLine("---------------------------------------------------------------");
+Console.WriteLine("Pilha");
+static void Pilha()
+{
+    Stack<int> pilha = new Stack<int>();
+    pilha.Push(1);
+    pilha.Push(2);
+    pilha.Push(3);
+    foreach (var item in pilha)
+    {
+        Console.WriteLine(item);
+    }
+
+    Console.WriteLine($"Removendo \n{pilha.Pop()}");
+    foreach (var item in pilha)
+    {
+        Console.WriteLine(item);
+    }
+
+}
+
+Pilha();
+
