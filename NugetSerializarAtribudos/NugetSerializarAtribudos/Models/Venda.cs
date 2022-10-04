3e13d87af93bfc5c728c3c0a198e4d84cd12177e
LucasArrabal <101684353+LucasArrabal@users.noreply.github.com>
2022-10-04 14:46:32 -0300

    Add files via upload

---
+++ NugetSerializarAtribudos/NugetSerializarAtribudos/Models/Venda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetSerializarAtribudos.Models
+{
+    public class Venda
+    {
+        public int Id { get; set; }
+        public string Produto { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DateVenda { get; set; }
+
+
+        public Venda(int id, string produto, decimal preco, DateTime dateVenda)
+        {
+            Id = id;
+            Produto = produto;
+            Preco = preco;
+            DateVenda = dateVenda;
+        }
+    }
+}
---
+++ NugetSerializarAtribudos/NugetSerializarAtribudos/Program.cs
@@ -0,0 +1,21 @@
+﻿using NugetSerializarAtribudos.Models;
+using Newtonsoft.Json;
+
+DateTime dataAtual = DateTime.Now;
+
+List<Venda> listaVenda = new List<Venda>();
+Venda v1 = new Venda(1,"Blusa",20.00M, dataAtual);
+Venda v2 = new Venda(2,"Calça", 25.00M, dataAtual);
+
+listaVenda.Add(v1);
+listaVenda.Add(v2);
+
+
+string teste = JsonConvert.SerializeObject(listaVenda, Formatting.Indented);
+
+
+File.WriteAllText("Arquivos/vendas.json", teste);
+Console.WriteLine(teste);
+Console.ReadLine();
+
+
